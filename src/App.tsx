ed63f9242bb08c9c5abb33f9b6e6074bcab30061
Palina Mitskevich <palina.mitskevich@gmail.com>
2022-11-02 19:16:42 +0100

    project initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
+import { Home } from './pages/Home'
+import { Picture } from './pages/Picture'
+import { paths } from './constants'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Welcome to React Router!</h1>
+        <Routes>
+          <Route path={paths.HOME} element={<Home />} />
+          <Route path={paths.PICTURE} element={<Picture />} />
+        </Routes>
+      </div>
   );
 }
 
---
+++ src/pages/Picture/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Picture: React.FC = () => {
+    return <div>picture page</div>
+}
